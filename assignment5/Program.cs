b8081a424d68b6952f0291d5918e47744c0c91b3
Fadyatef45 <ateffady98@gmail.com>
2026-02-20 00:12:45 +0200

    enums question1

--- assignment5/Program.cs
+++ assignment5/Program.cs
@@ -1,10 +1,49 @@
 ﻿namespace assignment5
 {
+    #region enums question1
+
     internal class Program
     {
+        enum DayOfWeek
+        {
+            Saturday = 1,
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a day number (1-7): ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 1 && number <= 7)
+            {
+                DayOfWeek day = (DayOfWeek)number;
+                Console.WriteLine($"Day: {day}");
+
+                switch (day)
+                {
+                    case DayOfWeek.Saturday:
+                    case DayOfWeek.Sunday:
+                        Console.WriteLine("It's the Weekend");
+                        break;
+
+                    default:
+                        Console.WriteLine("It's a Workday");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid day number!");
+            }
         }
     }
+
+    #endregion
 }
